ca4f66472c4fdb8308ed9cfdcbb6f9dbf6deb77b
peter <League@AdministorsiMac.attlocal.net>
2019-01-22 18:31:44 -0800

    teAPARTY

--- src/TeaParty.java
+++ src/TeaParty.java
@@ -1,4 +1,21 @@
 
 public class TeaParty {
-
+private String name ;
+private Boolean isWoman;
+private Boolean isKnighted;
+       public String welcome(String name, boolean isWoman, boolean isKnighted)
+       {
+    	   if(isWoman) {
+	if(isKnighted) {
+		return "Hello lady " + name;
+	}
+return "Hello Ms. " + name;
+       }
+    	   else {
+    		   if(isKnighted) {
+    			   return "Hello Sir " + name;
+    		   }
+    	   }
+return "Hello Mr. " + name;
+}
 }
